2218062ad8dc2ebf9693ad3443f48be32280eade
KaoruH <tatiane_tanizaki@hotmail.com>
2020-07-23 20:51:28 -0300

    Ejercicio 2.1

---
+++ bootcampg1/src/main/java/mvn org/apache/maven/plugins:maven-archetype-plugin:3/1/2:generate -DarchetypeArtifactId="maven-archetype-quickstart" -DarchetypeGroupId="org/apache/maven/archetypes" -DarchetypeVersion="1/4"/App.java
@@ -0,0 +1,13 @@
+package mvn org.apache.maven.plugins:maven-archetype-plugin:3.1.2:generate -DarchetypeArtifactId="maven-archetype-quickstart" -DarchetypeGroupId="org.apache.maven.archetypes" -DarchetypeVersion="1.4";
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ bootcampg1/src/test/java/mvn org/apache/maven/plugins:maven-archetype-plugin:3/1/2:generate -DarchetypeArtifactId="maven-archetype-quickstart" -DarchetypeGroupId="org/apache/maven/archetypes" -DarchetypeVersion="1/4"/AppTest.java
@@ -0,0 +1,20 @@
+package mvn org.apache.maven.plugins:maven-archetype-plugin:3.1.2:generate -DarchetypeArtifactId="maven-archetype-quickstart" -DarchetypeGroupId="org.apache.maven.archetypes" -DarchetypeVersion="1.4";
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
