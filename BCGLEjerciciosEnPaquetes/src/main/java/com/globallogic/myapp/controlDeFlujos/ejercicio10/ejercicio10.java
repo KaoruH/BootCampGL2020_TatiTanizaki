a27b2f787f1c8c68703892357e350daad1e51254
KaoruH <tatiane_tanizaki@hotmail.com>
2020-07-24 16:25:18 -0300

    Agregado ejercicio 10 de control de flujos y arreglado el ejercicio 6.

---
+++ BCGLEjerciciosEnPaquetes/src/main/java/com/globallogic/myapp/controlDeFlujos/ejercicio10/ejercicio10.java
@@ -0,0 +1,17 @@
+package com.globallogic.myapp.controlDeFlujos.ejercicio10;
+
+public class ejercicio10 {
+
+    public static void main(String[] args) {
+
+        String[] diasDeLaSemana = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
+
+        for (String string : diasDeLaSemana) {
+
+            System.out.println("Dia de la semana: " + string);
+            
+        }
+
+    }
+
+}
--- BCGLEjerciciosEnPaquetes/src/main/java/com/globallogic/myapp/controlDeFlujos/ejercicio6/ejercicio6.java
+++ BCGLEjerciciosEnPaquetes/src/main/java/com/globallogic/myapp/controlDeFlujos/ejercicio6/ejercicio6.java
@@ -6,12 +6,12 @@ public class ejercicio6 {
 
         String[] diasDeLaSemana = {"Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado", "Domingo"};
 
-        for (String string : diasDeLaSemana) {
+        for (int i = 0; i < diasDeLaSemana.length; i++) {
 
-            System.out.println("Dia de la semana: " + string);
+            System.out.println("Dia de la semana: " + diasDeLaSemana[i]);
             
         }
 
     }
-
+    
 }
